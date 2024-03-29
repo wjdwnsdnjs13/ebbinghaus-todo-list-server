bd29e5a6ceea1aa5f09a183d43399aee5455bdde
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-03-29 15:18:42 +0900

    :tada: feat: start project

---
+++ src/main/java/com/toy_project/ebbinghaustodolistserver/EbbinghausTodoListServerApplication.java
@@ -0,0 +1,13 @@
+package com.toy_project.ebbinghaustodolistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbbinghausTodoListServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbbinghausTodoListServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy_project/ebbinghaustodolistserver/EbbinghausTodoListServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy_project.ebbinghaustodolistserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbbinghausTodoListServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
